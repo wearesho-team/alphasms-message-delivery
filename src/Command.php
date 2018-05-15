e6a898ac5267fe0b90b842618fcc52738e428080
Alexander Letnikow <reclamme@gmail.com>
2018-05-15 15:22:52 +0300

    Add AlphaSms\Command interface

---
+++ src/Command.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Wearesho\Delivery\AlphaSms;
+
+/**
+ * Interface Command
+ * @package Wearesho\Delivery\AlphaSms
+ */
+interface Command
+{
+    public const SEND = 'send';
+    public const BALANCE = 'balance';
+}
