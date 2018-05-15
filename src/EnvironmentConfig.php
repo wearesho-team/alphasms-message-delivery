b1a291e35f04296040e5ac4ea6f6e1d44e6c8bea
Alexander Letnikow <reclamme@gmail.com>
2018-05-15 15:25:31 +0300

    Implement AlphaSms\ConfigInterface in AlphaSms\EnvironmentConfig using horat1us/environment-config

---
+++ src/EnvironmentConfig.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Wearesho\Delivery\AlphaSms;
+
+use Horat1us\Environment;
+
+/**
+ * Class EnvironmentConfig
+ * @package Wearesho\Delivery\AlphaSms
+ */
+class EnvironmentConfig extends Environment\Config implements ConfigInterface
+{
+    public function __construct(string $keyPrefix = 'ALPHASMS_')
+    {
+        parent::__construct($keyPrefix);
+    }
+
+    public function getLogin(): ?string
+    {
+        return $this->getEnv('LOGIN', [$this, 'null']);
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->getEnv('PASSWORD', [$this, 'null']);
+    }
+
+    public function getApiKey(): ?string
+    {
+        return $this->getEnv('API_KEY', [$this, 'null']);
+    }
+}
