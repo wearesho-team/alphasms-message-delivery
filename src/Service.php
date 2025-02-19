a3afdde6ae019941cc020dfce15b3057b2d4ab12
horat1us <reclamme+github@gmail.com>
2025-02-19 21:35:19 +0100

    Implement batch and send method in Service, add tests and console command

--- src/Service.php
+++ src/Service.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Wearesho\Delivery\AlphaSms;
 
+use Carbon\Carbon;
 use Psr\Http\Message\ResponseInterface;
 use Wearesho\Delivery;
 use GuzzleHttp;
@@ -11,6 +12,9 @@ use GuzzleHttp;
 class Service implements Delivery\Batch\ServiceInterface
 {
     public const NAME = 'alphasms';
+
+    public const CHANNEL_SMS = 'sms';
+    public const CHANNEL_VIBER = 'viber';
 
     protected const BASE_URI = 'https://alphasms.ua/api/json.php';
 
@@ -62,17 +66,156 @@ class Service implements Delivery\Batch\ServiceInterface
         );
     }
 
-    public function batch(iterable $messages): iterable
-    {
-        // TODO: Implement batch() method.
-    }
-
     public function send(Delivery\MessageInterface $message): Delivery\ResultInterface
     {
-        // TODO: Implement send() method.
+        [$result] = [...$this->batch([$message])];
+        return $result;
+    }
+
+    /**
+     * @param iterable $messages
+     * @return iterable<Delivery\MessageInterface>
+     * @throws Delivery\Exception
+     */
+    public function batch(iterable $messages): iterable
+    {
+        $messagesArray = [];
+        $requests = [];
+        $i = 0;
+        $time = Carbon::now()->getTimestamp();
+        foreach ($messages as $message) {
+            $key = "i_" . $time . '_' . $i++;
+            $messagesArray[$key] = $message;
+            $requests[] = $this->getRequestBody($key, $message);
+        }
+
+        $response = $this->sendRequest($requests);
+        if (count($response) !== count($messagesArray)) {
+            throw new Delivery\Exception(
+                "Invalid response messages count, "
+                . count($response) . " given, "
+                . count($messagesArray) . " expected.",
+                7001
+            );
+        }
+
+        foreach ($response as $responseItem) {
+            yield $this->parseResponseItem($responseItem, $messagesArray);
+        }
     }
 
     // region Internal API methods
+    protected function getRequestBody(string $id, Delivery\MessageInterface $message): array
+    {
+        $senderName = Delivery\Options::get($message, Delivery\Options::SENDER_NAME)
+            ?? $this->config->getSenderName();
+        $messageChannel = Delivery\Options::get($message, Delivery\Options::CHANNEL)
+            ?? self::CHANNEL_SMS;
+        $this->validateChannelName($messageChannel);
+
+        $request = [
+            'id' => $id,
+            'type' => $messageChannel,
+            'phone' => $message->getRecipient(),
+        ];
+        $webhookUrl = Delivery\Options::get($message, Options::WEBHOOK_URL)
+            ?? $this->config->getWebhookUrl();
+        if (!is_null($webhookUrl)) {
+            $request['hook'] = $webhookUrl;
+        }
+
+        $channelDependentOptions = [
+            'signature' => $senderName,
+            'message' => $message->getText(),
+        ];
+        if (!is_null($ttl = Delivery\Options::get($message, Delivery\Options::TTL))) {
+            $channelDependentOptions['lifetime'] = $ttl;
+        }
+
+        foreach ($channelDependentOptions as $option => $value) {
+            $request[$messageChannel . '_' . $option] = $value;
+        }
+        if ($messageChannel === self::CHANNEL_VIBER) {
+            $request['viber_type'] = 'text';
+        }
+        return $request;
+    }
+
+    private function parseResponseItem(array $responseItem, array &$messagesArray): Delivery\ResultInterface
+    {
+        if (!array_key_exists('success', $responseItem)) {
+            throw new Delivery\Exception(
+                "Missing success key in responseItem",
+                8001
+            );
+        }
+        if (!array_key_exists('data', $responseItem)) {
+            throw new Delivery\Exception(
+                "Missing data key in responseItem",
+                8002
+            );
+        }
+        if (!array_key_exists('id', $responseItem['data'])) {
+            throw new Delivery\Exception(
+                "Missing id key in responseItem data",
+                8003
+            );
+        }
+        $message = $messagesArray[$responseItem['data']['id']];
+        if (is_null($message)) {
+            throw new Delivery\Exception(
+                "Invalid id in responseItem data",
+                8004
+            );
+        }
+        return ($responseItem['success'] !== true)
+            ? $this->parseResponseItemFailure($responseItem, $message)
+            : $this->parseResponseItemSuccess($responseItem, $message);
+    }
+
+    private function parseResponseItemFailure(
+        array $responseItem,
+        Delivery\MessageInterface $message
+    ): Delivery\ResultInterface {
+        if (!array_key_exists('error', $responseItem)) {
+            throw new Delivery\Exception(
+                "Missing error key in responseItem",
+                8011
+            );
+        }
+
+        return new Delivery\Result(
+            messageId: $responseItem['data']['id'],
+            message: $message,
+            status: Delivery\Result\Status::Rejected,
+            reason: $responseItem['error']
+        );
+    }
+
+    private function parseResponseItemSuccess(
+        array $responseItem,
+        Delivery\MessageInterface $message
+    ): Delivery\ResultInterface {
+        if (!array_key_exists('msg_id', $responseItem['data'])) {
+            throw new Delivery\Exception(
+                "Missing msg_id key in responseItem data",
+                8101
+            );
+        }
+        return new Delivery\Result(
+            messageId: (string)$responseItem['data']['msg_id'],
+            message: $message,
+            status: Delivery\Result\Status::Accepted,
+        );
+    }
+
+    private function validateChannelName(string $channel): void
+    {
+        if (!in_array($channel, [self::CHANNEL_VIBER, self::CHANNEL_SMS])) {
+            throw new Delivery\Exception("Unsupported Channel: $channel", 10001);
+        }
+    }
+
     private function sendRequest(array $data): array
     {
         $body = [
