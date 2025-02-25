5206c30961164d24cf8c53adcdd5ab42e4d98084
horat1us <reclamme+github@gmail.com>
2025-02-25 16:51:41 +0100

    Add VoiceOtp Request entity

---
+++ src/VoiceOtp/Request.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Wearesho\Delivery\AlphaSms\VoiceOtp;
+
+class Request implements \JsonSerializable
+{
+    public const TYPE = 'call/otp';
+
+    public function __construct(
+        private readonly int $id,
+        private readonly string $phoneNumber,
+    ) {
+    }
+
+    public function id(): int
+    {
+        return $this->id;
+    }
+
+    public function phoneNumber(): string
+    {
+        return $this->phoneNumber;
+    }
+
+    final public function jsonSerialize(): array
+    {
+        return [
+            'type' => self::TYPE,
+            'id' => $this->id(),
+            'phoneNumber' => $this->phoneNumber(),
+        ];
+    }
+}
